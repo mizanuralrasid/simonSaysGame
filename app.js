b5a4427d01d12b22497a1ad31aa99777dacb2433
Mizanur Alrasid <88093931+mizanuralrasid@users.noreply.github.com>
2026-07-19 11:31:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ let idx = [];
 let h2 = document.querySelector("h2");
 let btns = ["red", "green", "yellow", "purple"];
 let gbtn = document.querySelectorAll(".btn");
+let msg = document.querySelector(".msg");
 let start = document.querySelector("button");
+function closePopUp(){
+    msg.classList.remove("dsblMsg");
+}
+function popUp(message){
+    msg.classList.add("dsblMsg");
+    msg.innerHTML = message || "Opps! Wrong button click, Click Start Button to Start the Game ↓";
+    let okBtn = document.createElement("button");
+    okBtn.setAttribute("class","okBtn");
+    okBtn.innerText = "X"
+    msg.appendChild(okBtn);
+    okBtn.addEventListener("click",closePopUp);
+ 
+}
 
 start.addEventListener('click', () => {
     started = true;
@@ -45,13 +59,25 @@ function checkAns(idx) {
             console.log("same value");
         }
     }
+    else if(started == false){
+            h2.innerText = "Opps! Wrong button click, Click Start Button to Start the Game";
+    }
     else {
         console.log("game over");
-        h2.innerText = `Your Score is ${level} press any key to start`;
+        h2.innerText = `GAME OVER!, Your Score is ${level} press any key to start`;
+        let body = document.querySelector("body");
+        body.classList.add("gameOver");
+        setTimeout(() => {
+            body.classList.remove("gameOver");
+        }, 200);
+        popUp(`GAME OVER!, Your Score is ${level} press any key to start`);
         reset();
     }
 }
 function btnPress() {
+    if (started == false) {
+        popUp();
+    }
     let btn = this;
     userFlash(btn);
     let userBtnColor = btn.getAttribute("id");
